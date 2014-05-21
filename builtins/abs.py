baea64079417a9a0dcbc4371ec4ac7aa1c18907e
spoty <spoty.sp@gmail.com>
2014-05-21 11:28:05 +0200

    Added      builtins/abs.py

---
+++ builtins/abs.py
@@ -0,0 +1,8 @@
+"""
+abs()
+
+Returns: absolute value of x - the positive distance between x and zero.
+"""
+print "abs(-45) : ", abs(-45)
+print "abs(100.12) : ", abs(100.12)
+print "abs(119L) : ", abs(119L)
