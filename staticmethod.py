ac9a2c2a7a3512ac0769ee45befd4d83c56e4fd3
spoty <spoty.sp@gmail.com>
2014-05-22 10:52:10 +0200

    Added      staticmethod.py

---
+++ staticmethod.py
@@ -0,0 +1,6 @@
+"""
+http://stackoverflow.com/questions/12179271/python-classmethod-and-staticmethod-for-beginner
+http://stackoverflow.com/questions/136097/what-is-the-difference-between-staticmethod-and-classmethod-in-python
+http://rapd.wordpress.com/2008/07/02/python-staticmethod-vs-classmethod/
+https://julien.danjou.info/blog/2013/guide-python-static-class-abstract-methods
+"""
