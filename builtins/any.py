270cc4897fcc36a4f154367e51a9446eb27730df
spoty <spoty.sp@gmail.com>
2014-05-23 15:41:04 +0200

    Added      builtins/any.py

---
+++ builtins/any.py
@@ -0,0 +1,51 @@
+"""
+You can roughly think of any and all as logical OR and AND operators,
+respectively.
+
+any
+
+any will return True when atleast one of the values is Truthy.
+Read about Truth Value Testing.
+
+all
+
+all will return True only when all the elements are Truthy.
+
+Truth table
+
+                        any     all
+-------------------------------------
+All Truthy values      True    True
+All Falsy values       False   False
+Atleast one Truthy     True    False
+Atleast one Falsy      True    False
+"""
+
+items = [[1, 2, 0], [1, 2, 0], [1, 2, 0]]
+print all(item[2] == 0 for item in items)
+# True
+items = [[1, 2, 0], [1, 2, 1], [1, 2, 0]]
+print all(item[2] == 0 for item in items)
+# False
+
+# And, for his filter example, a list comprehension:
+
+print [x for x in items if x[2] == 0]
+# [[1, 2, 0], [1, 2, 0]]
+
+# If you want to check at least one element is 0, the better option
+# is to use any() which is more readable:
+
+print any(item[2] == 0 for item in items)
+# True
+
+"""
+
+
+You could use itertools's takewhile like this, it will stop once a
+condition is met that fails your statement. The opposite method would be dropwhile
+
+for x in itertools.takewhile(lambda x: x[2] == 0, list)
+    print x
+
+"""
