c5ca78ceee2c5c5fe4ef2691c3248b727a2e06da
spotyx <spotyx@bitbucket.org>
2014-05-25 10:20:06 +0200

    Added      TOPICS/introspection.py

---
+++ TOPICS/introspection.py
@@ -0,0 +1,15 @@
+"""
+In computer programming, introspection refers to the ability to examine
+something to determine what it is, what it knows, and what it is capable
+of doing. Introspection gives programmers a great deal of flexibility and
+control. Once you've worked with a programming language that supports
+introspection, you may similarly feel that "the unexamined object is not
+worth instantiating."
+
+sourece:
+>> http://www.ibm.com/developerworks/library/l-pyint/index.html
+more:
+>> http://www.diveintopython.net/power_of_introspection/
+>> http://programmingexamples.wikidot.com/python-introspection
+>> http://zetcode.com/lang/python/introspection/
+"""
