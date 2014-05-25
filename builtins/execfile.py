e616e6284e70df17387fa4bf2458af9fe133a6b0
spoty <spoty.sp@gmail.com>
2014-05-25 18:37:47 +0200

    Added      builtins/execfile.py

---
+++ builtins/execfile.py
@@ -0,0 +1,8 @@
+""" execfile(filename)
+
+    This function is similar to the exec statement, but parses a file instead of
+    a string. It is different from the import statement in that it does not use
+    the module administration — it reads the file unconditionally and does not
+    create a new module.
+"""
+
