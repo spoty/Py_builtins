f3b0202c0c1cb24cd3b8aec4ac6adfa29170a6c8
spoty <spoty.sp@gmail.com>
2014-05-25 19:35:25 +0200

    Added      builtins/super.py

---
+++ builtins/super.py
@@ -0,0 +1,11 @@
+""" Class inheritance
+
+http://rhettinger.wordpress.com/2011/05/26/super-considered-super/
+
+  It helps to handle inheritance of ancestor class. This sounds a bit of messy.
+  So I'll try to explain.
+
+You have class A that does some useful things. And you have to use this class A
+everywhere in your application with adding some piece of functionality. You may
+inherit this class B(A) and expand it's functionality. Now you can use class B
+instead of class A everywhere and get rid of redundant operations. """
