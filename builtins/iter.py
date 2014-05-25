c5c854dccdbbe75e1b5cc404e53c9c9f71b44cc4
spoty <spoty.sp@gmail.com>
2014-05-25 23:28:20 +0200

    Added      builtins/iter.py

---
+++ builtins/iter.py
@@ -0,0 +1,4 @@
+"""http://stackoverflow.com/questions/19151/build-a-basic-python-iterator
+
+http://stackoverflow.com/questions/9884132/understanding-pythons-iterator-
+iterable-and-iteration-protocols-what-exact"""
