c17c13152569be0c216d0dc56dd68bc37c21b280
spoty <spoty.sp@gmail.com>
2014-05-26 11:01:56 +0200

    Classes

---
+++ builtins/classes/property.py
@@ -0,0 +1,6 @@
+"""http://stackoverflow.com/questions/6304040/real-world-example-about-how-to-use-
+property-feature-in-python
+
+
+http://blaag.haard.se/What-s-the-point-of-properties-in-Python/
+"""
