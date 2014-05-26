8750f4a56468f5a9efa2350c3f1878441ee0aa59
spoty <spoty.sp@gmail.com>
2014-05-26 12:18:33 +0200

    Added      builtins/type.py

---
+++ builtins/type.py
@@ -0,0 +1,6 @@
+""" type(name,bases,dict)
+
+    With one argument, return the type of an object. The return value is a type
+    object. The isinstance() built-in function is recommended for testing the
+    type of an object.
+"""
