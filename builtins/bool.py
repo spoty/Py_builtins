fa2bd090fb7ba2c5e50675c04b42f55674dc0a11
spoty <spoty.sp@gmail.com>
2014-05-26 12:06:52 +0200

    Added      builtins/bool.py

---
+++ builtins/bool.py
@@ -0,0 +1,9 @@
+"""
+bool([x])
+
+    Convert a value to a Boolean, using the standard truth testing procedure. If
+    x is false or omitted, this returns False; otherwise it returns True. bool
+    is also a class, which is a subclass of int. Class bool cannot be subclassed
+    further. Its only instances are False and True.
+
+"""
