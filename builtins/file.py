3d70f82aadbd7609b1476b6d9eb1d3c40fa0c5c5
spoty <spoty.sp@gmail.com>
2014-05-26 10:44:17 +0200

    Added      builtins/file.py

---
+++ builtins/file.py
@@ -0,0 +1,10 @@
+"""When opening a file, it's preferable to use open() instead of invoking this
+constructor directly. file is more suited to type testing (for example, writing
+"isinstance(f, file)").
+
+
+In python 3.0 file WAS moveD from being a built-in to being implemented
+by multiple classes in the io library.
+
+"""
+
