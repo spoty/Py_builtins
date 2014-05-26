34c606fd5a5085e1d75324c72d3c185bcf04bb62
spoty <spoty.sp@gmail.com>
2014-05-26 11:01:26 +0200

    Moved to folder: classes

--- builtins/iter.py
+++
@@ -1,4 +0,0 @@
-"""http://stackoverflow.com/questions/19151/build-a-basic-python-iterator
-
-http://stackoverflow.com/questions/9884132/understanding-pythons-iterator-
-iterable-and-iteration-protocols-what-exact"""
