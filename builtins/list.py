47d3e51058656a921532815264532491e8735d36
spoty <spoty.sp@gmail.com>
2014-05-27 16:08:44 +0200

    Added      builtins/list.py

---
+++ builtins/list.py
@@ -0,0 +1,12 @@
+list([iterable])
+
+    Return a list whose items are the same and in the same order as iterable‘s
+    items. iterable may be either a sequence, a container that supports
+    iteration, or an iterator object. If iterable is already a list, a copy is
+    made and returned, similar to iterable[:]. For instance, list('abc') returns
+    ['a', 'b', 'c'] and list( (1, 2, 3) ) returns [1, 2, 3]. If no argument is
+    given, returns a new empty list, [].
+
+    list is a mutable sequence type, as documented in Sequence Types — str,
+    unicode, list, tuple, bytearray, buffer, xrange. For other containers see
+    the built in dict, set, and tuple classes, and the collections module.
