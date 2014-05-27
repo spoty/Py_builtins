c4a89cf91b41ee21c22af2ad67b51d70f7e92f4c
spoty <spoty.sp@gmail.com>
2014-05-27 14:25:04 +0200

    Added      builtins/cmp.py

---
+++ builtins/cmp.py
@@ -0,0 +1,11 @@
+"""
+cmp(x, y)
+
+    Compare the two objects x and y and return an integer according to the
+    outcome. The return value is negative if x < y, zero if x == y and strictly
+    positive if x > y.
+
+    Python3:
+    __lt__ and __eq__ method for ordering in Python 3.
+    __cmp__ is no longer used.
+"""
