1c9fedc741f1e1c42a334be3603bec1153e4907a
spoty <spoty.sp@gmail.com>
2014-05-28 15:16:37 +0200

    Added      builtins/chr.py

---
+++ builtins/chr.py
@@ -0,0 +1,6 @@
+chr(i)
+
+    Return a string of one character whose ASCII code is the integer i. For
+    example, chr(97) returns the string 'a'. This is the inverse of ord(). The
+    argument must be in the range [0..255], inclusive; ValueError will be raised
+    if i is outside that range. See also unichr().
