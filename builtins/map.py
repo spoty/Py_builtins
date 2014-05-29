1b52d55bfe9a4b73c05404a77abcae800934cbd6
spoty <spoty.sp@gmail.com>
2014-05-29 11:08:10 +0200

    Added      builtins/map.py

---
+++ builtins/map.py
@@ -0,0 +1,12 @@
+map(function, iterable, ...)
+
+    Apply function to every item of iterable and return a list of the results.
+    If additional iterable arguments are passed, function must take that many
+    arguments and is applied to the items from all iterables in parallel. If one
+    iterable is shorter than another it is assumed to be extended with None
+    items. If function is None, the identity function is assumed; if there are
+    multiple arguments, map() returns a list consisting of tuples containing the
+    corresponding items from all iterables (a kind of transpose operation). The
+    iterable arguments may be a sequence or any iterable object; the result is
+    always a list.
+
