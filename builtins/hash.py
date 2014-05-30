a45bdac10dee8dc1be81e14dc693263f3105aa31
spoty <spoty.sp@gmail.com>
2014-05-30 13:10:26 +0200

    Added      builtins/hash.py

---
+++ builtins/hash.py
@@ -0,0 +1,7 @@
+
+hash(object)
+
+    Return the hash value of the object (if it has one). Hash values are
+    integers. They are used to quickly compare dictionary keys during a
+    dictionary lookup. Numeric values that compare equal have the same hash
+    value (even if they are of different types, as is the case for 1 and 1.0).
